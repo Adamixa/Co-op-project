83f0e5be31c851d92fa0699ea70fa51cc719a726
Abdulaziz-Eid <al-otaibi-41@hotmail.com>
2023-08-08 16:35:16 +0300

    incomplete Vote and Rating

---
+++ UniversityResturantInformation/Controllers/RatingController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UniversityResturantInformation.Controllers
+{
+    public class RatingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Rating()
+        {
+            return View();
+        }
+    }
+}
---
+++ UniversityResturantInformation/Controllers/RatingsController.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using UniversityResturantInformation.Models;
+
+namespace UniversityResturantInformation.Controllers
+{
+    public class RatingsController : Controller
+    {
+        private readonly RestaurantDB _context;
+
+        public RatingsController(RestaurantDB context)
+        {
+            _context = context;
+        }
+
+        // GET: Ratings
+        public async Task<IActionResult> Index()
+        {
+            var restaurantDB = _context.Ratings.Include(r => r.Item).Include(r => r.User);
+            return View(await restaurantDB.ToListAsync());
+        }
+
+        
+            public async Task<IActionResult> Rate(int id, [Bind("Id,UserId,ItemId,Rate")] Item item)
+            {
+                if (id != item.Id)
+                {
+                    return NotFound();
+                }
+                ViewData["ItemId"] = new SelectList(_context.Items, "Id", "Name", item.ItemName);
+                return View(item);
+            }
+        
+
+
+
+
+        // GET: Ratings/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var rating = await _context.Ratings
+                .Include(r => r.Item)
+                .Include(r => r.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (rating == null)
+            {
+                return NotFound();
+            }
+
+            return View(rating);
+        }
+
+        // GET: Ratings/Create
+        public IActionResult Create()
+        {
+            ViewData["ItemId"] = new SelectList(_context.Items, "Id", "Id");
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: Ratings/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,UserId,ItemId,Rate")] Rating rating)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(rating);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ItemId"] = new SelectList(_context.Items, "Id", "Id", rating.ItemId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", rating.UserId);
+            return View(rating);
+        }
+
+        // GET: Ratings/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var rating = await _context.Ratings.FindAsync(id);
+            if (rating == null)
+            {
+                return NotFound();
+            }
+            ViewData["ItemId"] = new SelectList(_context.Items, "Id", "Id", rating.ItemId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", rating.UserId);
+            return View(rating);
+        }
+
+        // POST: Ratings/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,UserId,ItemId,Rate")] Rating rating)
+        {
+            if (id != rating.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(rating);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RatingExists(rating.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ItemId"] = new SelectList(_context.Items, "Id", "Id", rating.ItemId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", rating.UserId);
+            return View(rating);
+        }
+
+        // GET: Ratings/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var rating = await _context.Ratings
+                .Include(r => r.Item)
+                .Include(r => r.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (rating == null)
+            {
+                return NotFound();
+            }
+
+            return View(rating);
+        }
+
+        // POST: Ratings/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var rating = await _context.Ratings.FindAsync(id);
+            _context.Ratings.Remove(rating);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RatingExists(int id)
+        {
+            return _context.Ratings.Any(e => e.Id == id);
+        }
+    }
+}
